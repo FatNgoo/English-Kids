7c84ea888012615656858802ea2c198dcf8dd68f
NgooFat <ngoophat2005@gmail.com>
2025-12-18 12:48:08 +0700

    add some picture

--- app/src/main/java/com/edu/english/MainActivity.java
+++ app/src/main/java/com/edu/english/MainActivity.java
@@ -12,18 +12,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        
+        // Initialize DrawerLayout
+        drawerLayout = findViewById(R.id.drawer_layout);
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Setup profile click to open drawer
+        LinearLayout profileSection = findViewById(R.id.profile_section);
+        profileSection.setOnClickListener(v -> {
+            if (drawerLayout != null) {
+                drawerLayout.openDrawer(android.view.Gravity.START);
+            }
         });
 
         // Setup card click listeners
